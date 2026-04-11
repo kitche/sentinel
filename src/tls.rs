f83e815879f47adcb34c1e705e3dcf32ff17fd92
kitche <kitchetech@gmail.com>
2026-04-10 22:08:00 -0500

    Load TLS cert and key from configured files

--- src/tls.rs
+++ src/tls.rs
@@ -1,19 +1,25 @@
 use rustls::ServerConfig as TlsServerConfig;
+use rustls::pki_types::{CertificateDer, PrivateKeyDer, pem::PemObject};
 use tokio_rustls::TlsAcceptor;
 use std::sync::Arc;
 use crate::config::TlsConfig;
 
 pub fn create_tls_acceptor(tls_config: &TlsConfig) 
     -> Result<TlsAcceptor, Box<dyn std::error::Error>> {
-    let cert = rcgen::generate_simple_self_signed(vec!["localhost".to_string()])?;
-    let key = rustls::pki_types::PrivateKeyDer::try_from(
-        cert.key_pair.serialize_der()
-    )?;
-    let cert_der = rustls::pki_types::CertificateDer::from(cert.cert);
+    let cert_pem = std::fs::read(&tls_config.cert_path)?;
+    let certs: Vec<CertificateDer<'static>> =
+        CertificateDer::pem_slice_iter(&cert_pem).collect::<Result<Vec<_>, _>>()?;
+
+    if certs.is_empty() {
+        return Err("No certificates found in cert_path".into());
+    }
+
+    let key_pem = std::fs::read(&tls_config.key_path)?;
+    let key = PrivateKeyDer::from_pem_slice(&key_pem)?;
 
     let mut server_config = TlsServerConfig::builder()
         .with_no_client_auth()
-        .with_single_cert(vec![cert_der], key)?;
+        .with_single_cert(certs, key)?;
 
     if tls_config.enable_http2 {
         server_config.alpn_protocols = vec![b"h2".to_vec(), b"http/1.1".to_vec()];
